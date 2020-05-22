b611416fd9eb1163fbb73dfc451f096090392608
Sachin Shetty <sachinshettymumbai@gmail.com>
2020-05-23 01:12:31 +0530

    Update Unzip.py

--- Unzip.py
+++ Unzip.py
@@ -1,6 +1,12 @@
 from zipfile import ZipFile
 import argparse
 
+#Pass the full path of the zip file to be extracted(Source)
+#Pass the full path where the conents of the zip file will be extracted(Destination)
+#Assuming that the unzip.py file is in c:\myfiles folder. 
+#The below is the example for the command to be typed in the Dos prompt. I am assuming that you have set your python path in the environment variable
+#python C:\myfiles\unzip.py "c:\FilestoUnzip\abcd.zip" "c:\unzip"
+#Replace the source and destination as per your requirement
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('SourceZippath',metavar='N', type=str,help='Enter zip filename with path')
