007fce574b65c996211f57465bab06bff82b6a7a
Sachin Shetty <sachinshettymumbai@gmail.com>
2020-05-25 17:37:59 +0530

    Delete csvtoxlsx.py

--- Proj/csvtoxlsx.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-read_file = pd.read_csv (r'C:\MyFiles\Dhaval\GSM\20200506.PDBTxnDlyTD.csv')
-read_file.to_excel (r'C:\MyFiles\Dhaval\GSM\20200506.PDBTxnDlyTD.xlsx', index = None, header=True)
